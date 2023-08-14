b9fb8a40e379ae1f8a71c687aa3b9deb33b2dce6
K4uan1 <142123948+K4uan1@users.noreply.github.com>
2023-08-14 14:48:21 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const scrollButtons = document.querySelectorAll('.scrollButton');
 searchInput.addEventListener('input', () => {
   const query = searchInput.value.trim().toLowerCase();
 
+  scrollButtons.forEach(button => {
+    const buttonNumber = button.getAttribute('data-number');
+    if (query === '' || buttonNumber.includes(query)) {
+      button.style.display = '';
+    } else {
+      button.style.display = '';
+    }
+  });
+});
 
+searchInput.addEventListener('focusout', () => {
+  if (searchInput.value.trim() === '') {
+    scrollButtons.forEach(button => {
+      button.style.display = '';
+    });
+  }
 });
 
