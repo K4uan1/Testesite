f5833e627a5c8b9881a52b2a5ae9a63f611bb9da
K4uan1 <142123948+K4uan1@users.noreply.github.com>
2023-08-14 14:45:06 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ const scrollButtons = document.querySelectorAll('.scrollButton');
 searchInput.addEventListener('input', () => {
   const query = searchInput.value.trim().toLowerCase();
 
-  scrollButtons.forEach(button => {
-    const buttonNumber = button.getAttribute('data-number');
-    if (buttonNumber.includes(query)) {
-      button.style.display = '';
-    } else {
-      button.style.display = '';
-    }
-  });
+
 });
 
