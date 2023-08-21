c1674b562529d57ba55e2e5af4b198fe6ac63b38
COA <142123948+K4uan1@users.noreply.github.com>
2023-08-21 08:10:01 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ window.onclick = function(event) {
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
+      if
+(openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
