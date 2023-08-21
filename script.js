288f50ace9dd0e456f0722f66479267666ff7738
COA <142123948+K4uan1@users.noreply.github.com>
2023-08-21 07:22:49 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const menuButton = document.querySelector(".menu-button");
 
   menuButton.addEventListener("click", function() {
-    this.classList.toggle("active");
+    const menuOptions = document.querySelector(".menu-options");
+    menuOptions.style.display = menuOptions.style.display === "block" ? "none" : "block";
   });
 });
